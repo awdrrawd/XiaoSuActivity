ea2bc7881148077738ea6898f9567746bdceeead
Likolisu <58585499+awdrrawd@users.noreply.github.com>
2026-08-26 08:27:45 +0800

    Refactor match patterns in userLoad_dev.user.js
    Updated match patterns to a single include regex.

--- dev/userLoad_dev.user.js
+++ dev/userLoad_dev.user.js
@@ -4,10 +4,7 @@
 // @version 0.0.1
 // @description 小酥的动作拓展 一些额外的动作
 // @author XiaoSu
-// @match https://bondageprojects.elementfx.com/*
-// @match https://www.bondageprojects.elementfx.com/*
-// @match https://bondage-europe.com/*
-// @match https://www.bondage-europe.com/*
+// @include /^https:\/\/(www\.)?(bondage(projects\.elementfx|-(europe|asia))\.com|bondageeurope\.com)\/R*/
 // @run-at document-end
 // @grant none
 // ==/UserScript==
