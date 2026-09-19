e78ea3c90d3a02f696727e9ca0ff8e76ed4c9bf3
awdrrawd <awdrrawd@users.noreply.github.com>
2026-09-19 06:57:24 +0000

    deploy: d8caebc08552282cf20253452abd7c9cf871bf43

--- dev/userLoad_dev.user.js
+++ dev/userLoad_dev.user.js
@@ -4,7 +4,7 @@
 // @version 0.0.1
 // @description 小酥的动作拓展 一些额外的动作
 // @author XiaoSu
-// @include /^https:\/\/(www\.)?(bondage(projects\.elementfx|-(europe|asia))\.com|bondageeurope\.com)\/R*/
+// @include  /^https:\/\/(www\.)?(bondage(projects\.elementfx|-(europe|asia))\.com|bondageeurope\.com)\/.*/
 // @run-at document-end
 // @grant none
 // ==/UserScript==
